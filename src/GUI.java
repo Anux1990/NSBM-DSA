00025920beb16a434873a5385375526fce0b799b
Anu <thilananupa@gmail.com>
2015-02-25 17:47:48 +0530

    initial commit

---
+++ src/GUI.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+
+public class GUI {
+	public void gui()
+	{
+		JFrame JF1 = new JFrame("Formal Foundation Project");
+		JF1.setVisible(true);
+		JF1.setSize(400,600);
+		JF1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
---
+++ src/testing.java
@@ -0,0 +1,8 @@
+//import javax.swing.*;
+public class testing {
+
+	public static void main(String[] args) {
+		GUI callGUI = new GUI();
+		callGUI.gui();
+	}
+}
