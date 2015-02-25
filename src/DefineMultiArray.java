e33c513eba72dea7b5d6c76b87af40f745123928
Anu <thilananupa@gmail.com>
2015-02-25 23:48:29 +0530

    Array i guess

--- src/DefineMultiArray.java
+++ src/DefineMultiArray.java
@@ -1,24 +1,28 @@
 
 public class DefineMultiArray {
-	int[][] residentialArray = new int [3][5];
+	String[][] residentialArray = new String [15][7];
 	public void printGrid()
 	{
-		/*int k = 1;
-		 for(int i=0;i<14;i++) {
-	            for(int j=0;j<14;j++) {
-	            	residentialArray[i][j]=k;
+		int k = 1;
+		 for(int i=0;i<15;i++) {
+	            for(int j=0;j<7;j++) {
+	            	String r = Integer.toString(k);
+	            	residentialArray[i][j]=r;
 	                k++;
 	            }
-	        }*/
+	        }
 
-		 for (int i = 0; i < residentialArray.length; i++) {
+		 for (int i = 0; i < residentialArray.length; i++) 
+		 {
 
 			    // Loop and display sub-arrays.
-			    int[] sub = residentialArray[i];
-			    for (int x = 0; x < sub.length; x++) {
-				System.out.print(sub[x] + " ");
+			 String[] sub = residentialArray[i];
+			    
+			    for (int x = 0; x < sub.length; x++)
+			    {
+			    	System.out.print(sub[x] + " ");
 			    }
 			    System.out.println();
-			}
-}
+		 }
+	}
 }
